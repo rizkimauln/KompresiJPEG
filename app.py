a70543e234a5e74946156ab544d8cae07806fe4a
Rizki Maulana <rm.rizki.mauln@gmail.com>
2026-06-23 16:17:44 +0700

    Fix CORS by increasing gunicorn timeout and ensuring wildcard CORS config

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from sklearn.decomposition import PCA, NMF
 
 app = Flask(__name__)
 # Mengizinkan aplikasi React (berbeda port) untuk mengakses API ini
-CORS(app, resources={r"/*": {"origins": "*"}}, supports_credentials=True)
+CORS(app, resources={r"/*": {"origins": "*"}})
 
 # ----------------- FUNGSI ALGORITMA KOMPRESI -----------------
 
